cf3712666e4fb61f2252b9412621aaa7ac5d8eea
Branislav <branislav@iMac.local>
2020-11-09 18:08:15 +0000

    adding items into db and updating item from db

---
+++ public/browser.js
@@ -0,0 +1,16 @@
+document.addEventListener("click", (ev) => {
+    if(ev.target.classList.contains("edit-me")){
+        // get user input from prompt and save it to the var
+        let userInput = prompt("Enter your desired new text:", ev.target.parentElement.parentElement.querySelector('.item-text').innerHTML);
+        if(userInput){
+            axios.post('/update-item', {text: userInput, id: ev.target.getAttribute("data-id")}).then(() => {
+                // traverse two steps up of the dom and select the span that contains text and change it for new one
+                ev.target.parentElement.parentElement.querySelector('.item-text').innerHTML = userInput;
+            }).catch(err => {
+                alert(err);
+            });
+        }else {
+            ev.target.parentElement.parentElement.querySelector('.item-text').innerHTML
+        }
+    }
+});
